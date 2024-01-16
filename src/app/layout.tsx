7623d0ae67bb1c4f01657301264ae5dc28836230
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-01-16 03:27:02 +0000

    config: google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js"></script>
+        <GoogleTagManager gtmId='G-KJY5EZVKZS'/>
+      </Head>
+      <body className={inter.className}>
+        {children}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+      </body>
     </html>
   )
 }
