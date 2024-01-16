5f3d1c29311fa908bfa5f482d7c959803b2270df
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-01-16 20:37:01 +0000

    feat: add i18n

---
+++ src/utils/schema.ts
@@ -0,0 +1,99 @@
+export type NavItemProps = {
+    title: string;
+    link: string;
+};
+
+export type HeaderProps = {
+  title: string;
+  text: string;
+};
+
+export type ServiceItemProps = {
+  title: string;
+  text: string;
+  image: string;
+};
+
+export type ServiceProps = {
+  title: string;
+  text: string;
+  items: ServiceItemProps[];
+};
+
+export type PortfolioItemProps = {
+  title: string;
+  tags: string[];
+  description: string;
+  image: string;
+  repo: string;
+  icon:string
+  link: string;
+};
+
+export type PortfolioProps = {
+  title: string;
+  text: string;
+  items: PortfolioItemProps[];
+  skills: {
+    languages: { title: string; image: string }[];
+    frameworks: { title: string; image: string }[];
+    databases: { title: string; image: string }[];
+  };
+};
+
+export type ExperienceItemProps = {
+  title: string;
+  company: string;
+  text: string;
+  start: string;
+  current: boolean;
+  end: string;
+  image: string;
+};
+
+export type ExperienceProps = {
+  title: string;
+  text: string;
+  items: ExperienceItemProps[];
+};
+
+export type ContactForm = {
+  name: string;
+  email: string;
+  phone: string;
+  company: string;
+  message: string;
+};
+
+export type ContactProps = {
+  title: string;
+  text: string;
+  form: ContactForm;
+  nameField: string;
+  emailField: string;
+  phoneField: string;
+  companyField: string;
+  messageField: string;
+};
+
+export type FooterProps = {
+  copyrightText: string;
+  links: {
+    title: string;
+    icon: string;
+    link: string;
+  }[];
+};
+
+export type Schema = {
+  nav: NavItemProps[];
+  header: HeaderProps;
+  services: ServiceProps;
+  portfolio: PortfolioProps;
+  experience: ExperienceProps;
+  contact: ContactProps;
+  footer: FooterProps;
+};
+
+export const langs = ['en','fr'] as const
+export type lang = typeof langs[number]
