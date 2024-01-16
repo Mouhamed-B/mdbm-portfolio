07a87f223f93d28125dda0295d66552b2424a37b
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-01-16 21:26:19 +0000

    edit header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 const Header = (props:HeaderProps) => {
   return (
-    <header className="masthead">
+    <header className="masthead h-100">
         <div className="container">
+            <div className="masthead-heading text-start text-primary">Mouhamed Bakhoum</div>
             <div className="masthead-subheading text-start" style={{fontWeight:"bold"}}>{props.title}</div>
             <p className="masthead-subheading text-start small w-75">{props.text}</p>
         </div>
