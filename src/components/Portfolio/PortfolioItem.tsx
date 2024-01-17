da841d773277ac14fbc744e4d96d434972c71098
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-01-17 19:07:53 +0000

    feat:update portfolio card

--- src/components/Portfolio/PortfolioItem.tsx
+++ src/components/Portfolio/PortfolioItem.tsx
@@ -8,19 +8,28 @@ const PortfolioItem = (props:PortfolioItemProps) => {
         return index
     }
     return (
-        <div className="portfolio-item">
-            <a className="portfolio-link" data-bs-toggle="modal" href="#portfolioModal1">
-                <div className="portfolio-hover">
-                    <div className="portfolio-hover-content"><i className="fas fa-plus fa-3x"></i></div>
+        <div className="portfolio-item w-100 h-100">
+            <div className="portfolio-caption p-4 rounded-5 shadow-sm w-100 h-100 d-flex flex-column justify-content-between gap-3">
+                <div className="portfolio-caption-heading text-secondary small">{props.title}</div>
+                <div>
+                    <div className="text-muted text-start mb-2" style={{height:'5em'}}>
+                    {props.description}
+                    </div>
+                    
+                    <img className="img-fluid object-fit-contain" src={props.image.length > 0 ? props.image : 'assets/img/portfolio/'+getIndex()+'.jpg'} alt={'Mouhamed Bakhoum '+props.title} height={200}/>
                 </div>
-                <img className="img-fluid" src={props.image.length > 0 ? props.image : 'assets/img/portfolio/'+getIndex()+'.jpg'} alt={'Mouhamed Bakhoum '+props.title} />
-            </a>
-            <div className="portfolio-caption">
-                <div className="portfolio-caption-heading">{props.title}</div>
-                <div className="portfolio-caption-subheading text-muted">{props.tags.join(', ')}</div>
+                <div className="row gap-2">
+                    {props.link.length>0 && <a href={props.link} className='text-primary text-lg-start '><div className="d-flex gap-2"><i className="fa-solid fa-link fa-lg"></i><span className="small">Live</span></div></a> }
+                    {props.repo.length>0 && <a href={props.repo} className='text-primary text-lg-start '><div className="d-flex gap-2"><i className="fa-brands fa-github fa-lg"></i><span>Github</span></div></a>}
+                </div>
+                <div className="row g-0 gap-2 mt-auto">
+                {props.tags.map((tag, index) => (
+                    <div className="badge text-bg-primary w-auto  mb-1" key={index}>{tag}</div>
+                ))}
+                </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default PortfolioItem
