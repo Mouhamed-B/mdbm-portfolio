21c1ec90792f0384fa914edd348a31fa61332b49
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-01-18 02:37:42 +0000

    content:add projects links

--- src/components/Portfolio/PortfolioItem.tsx
+++ src/components/Portfolio/PortfolioItem.tsx
@@ -18,9 +18,9 @@ const PortfolioItem = (props:PortfolioItemProps) => {
                     
                     <img className="img-fluid object-fit-contain" src={props.image.length > 0 ? props.image : 'assets/img/portfolio/'+getIndex()+'.jpg'} alt={'Mouhamed Bakhoum '+props.title} height={200}/>
                 </div>
-                <div className="row gap-2">
-                    {props.link.length>0 && <a href={props.link} className='text-primary text-lg-start '><div className="d-flex gap-2"><i className="fa-solid fa-link fa-lg"></i><span className="small">Live</span></div></a> }
-                    {props.repo.length>0 && <a href={props.repo} className='text-primary text-lg-start '><div className="d-flex gap-2"><i className="fa-brands fa-github fa-lg"></i><span>Github</span></div></a>}
+                <div className="row gap-2 mt-1">
+                    {props.link.length>0 && <a href={props.link} className='text-primary text-lg-start w-auto'><div className="d-flex gap-2"><i className="fa-solid fa-link fa-lg"></i><span>Live</span></div></a> }
+                    {props.repo.length>0 && <a href={props.repo} className='text-primary text-lg-start w-auto'><div className="d-flex gap-2"><i className="fa-brands fa-github fa-lg"></i><span>Github</span></div></a>}
                 </div>
                 <div className="row g-0 gap-2 mt-auto">
                 {props.tags.map((tag, index) => (
