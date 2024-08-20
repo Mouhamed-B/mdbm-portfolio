969c7a11069eb45c113e3a769aa45f9e17202123
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-08-20 14:52:03 +0000

    fix script closing tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Script strategy='afterInteractive' src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossOrigin="anonymous"></Script>
-        <Script strategy='afterInteractive' src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossOrigin="anonymous"></script>
+        <Script strategy='afterInteractive' src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossOrigin="anonymous"></Script>
         <script src="js/scripts.js"></script>
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js"></script>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-KJY5EZVKZS"></script>
