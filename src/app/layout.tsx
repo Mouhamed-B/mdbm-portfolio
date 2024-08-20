952662a36c0b7ff41cb8fd0bfe4fad3d18ce7ee3
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-08-20 14:27:35 +0000

    use nextjs script tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './fonts.css'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { lang, langs } from '@/utils/schema'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,8 +26,8 @@ export default function RootLayout({
     <html lang={params.lang}>
       <body className={inter.className}>
         {children}
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossOrigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossOrigin="anonymous"></script>
+        <Script strategy='afterInteractive' src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossOrigin="anonymous"></Script>
+        <Script strategy='afterInteractive' src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossOrigin="anonymous"></script>
         <script src="js/scripts.js"></script>
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js"></script>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-KJY5EZVKZS"></script>
